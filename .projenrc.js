b840dccf76ce1953da41ab57315a0a4f5b48cbb3
cloudadder <cloudadder.com@gmail.com>
2022-01-12 21:52:09 +1100

    no npm release

--- .projenrc.js
+++ .projenrc.js
@@ -13,6 +13,7 @@ const project = new awscdk.AwsCdkTypeScriptApp ({
   name: '@cloudadder/cdk-constructs-sandpit',
   release: true,
   releaseToNpm: false,
+  releaseEveryCommit: false,
 
   // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
   // deps: [],                    /* Runtime dependencies of this module. */
